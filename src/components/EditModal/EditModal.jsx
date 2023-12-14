9bae8a5f3ae0385678da3225c4d1c1da594ac25e
KananPy <kenan.semenderli@gmail.com>
2023-12-14 17:10:55 +0400

    done

--- src/components/EditModal/EditModal.jsx
+++ src/components/EditModal/EditModal.jsx
@@ -3,8 +3,7 @@ import { Button, Modal } from 'react-bootstrap';
 import { updateDoc, doc } from 'firebase/firestore';
 import { formDataCollection } from '../../firebase/firestore';
 import sectorData from '../../utils/data.json';
-import { toast } from "react-toastify";
-
+import { toast } from 'react-toastify';
 
 const EditModal = ({ isOpen, isClosed, data }) => {
   const [editedData, setEditedData] = useState({});
@@ -31,8 +30,8 @@ const EditModal = ({ isOpen, isClosed, data }) => {
   const handleSave = async () => {
     const docRef = doc(formDataCollection, data.id);
     await updateDoc(docRef, editedData);
-    toast.success("Data has been updated successfully!", {
-      position: "top-center",
+    toast.success('Data has been updated successfully!', {
+      position: 'top-center',
       autoClose: 3000,
     });
 
@@ -41,25 +40,27 @@ const EditModal = ({ isOpen, isClosed, data }) => {
 
   return (
     <Modal show={isOpen} onHide={isClosed} centered>
-      <Modal.Header closeButton>
+      <Modal.Header closeButton className="bg-[#88AB8E] text-white">
         <Modal.Title>Edit Data</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <label>
+        <label className="block mb-2">
           Name:
           <input
             type="text"
             name="name"
             value={editedData.name || ''}
             onChange={handleInputChange}
+            className="form-input mt-1 block w-full rounded-md border border-gray-300"
           />
         </label>
-        <label>
+        <label className="block mb-2">
           Sector:
           <select
             name="sector"
             value={editedData.sector || ''}
             onChange={handleInputChange}
+            className="form-select mt-1 block w-full rounded-md border-gray-300"
           >
             {renderOptions(sectorData)}
           </select>
@@ -67,13 +68,12 @@ const EditModal = ({ isOpen, isClosed, data }) => {
         {/* Add more input fields for other properties */}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={isClosed}>
+        <Button variant="secondary" onClick={isClosed} className="mr-2">
           Close
         </Button>
         <Button variant="primary" onClick={handleSave}>
           Save Changes
         </Button>
-       
       </Modal.Footer>
     </Modal>
   );
